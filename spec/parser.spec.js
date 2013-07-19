9eabb6e26c47647ba152c99c5dc711db9af2f79a
Dan Kang <dartdanfm@gmail.com>
2013-07-19 14:03:08 -0400

    Basic parser combinator implementation.  Also rewrote existing parsers with parser combinators.  Also wrote unit tests for all

--- spec/parser.spec.js
+++ spec/parser.spec.js
@@ -7,7 +7,79 @@ describe('parser', function () {
   var LPAREN = lexer.tokens.LPAREN;
   var RPAREN = lexer.tokens.RPAREN;
 
-  it('should parse sentences according to ValueSet production rule', function () {
+  it('empty should take a value and return a parser returns the value as parsed result', function () {
+    var emptyParser = parser.empty("The Man");
+    expect(emptyParser([1, 2, 5],0)).toEqual({ tree: "The Man", index: 0 });
+    expect(emptyParser([1, 2, 5],3)).toBe(false);
+  });
+
+  it('elem should return a parser that returns the matched element', function () {
+    var elemParser = parser.elem(LPAREN);
+    expect(elemParser([LPAREN, RPAREN], 0)).toEqual( { tree: LPAREN, index: 1});
+    expect(elemParser([LPAREN, RPAREN], 1)).toBe(false);
+  });
+
+  it('tuple2 should take two parsers and return a parser that runs them in order', function () {
+    var l = parser.elem(LPAREN);
+    var r = parser.elem(RPAREN);
+    var lr = parser.tuple2(l, r, function (a, b) { return [a, b]; });
+    expect(lr([LPAREN, RPAREN, 1, 2, 3], 0)).toEqual( { tree: [LPAREN, RPAREN], index: 2 });
+    expect(lr([LPAREN, 1, RPAREN, 1, 2, 3], 0)).toBe(false);
+  });
+
+
+  it('tupleMany should take an array of parsers and return a parser that runs them in order', function () {
+    var l = parser.elem(LPAREN);
+    var r = parser.elem(RPAREN);
+    var lrlr = parser.tupleMany([l, r, l, r]);
+    expect(lrlr([1, LPAREN, RPAREN, LPAREN, RPAREN], 1)).toEqual({
+      tree: [LPAREN, RPAREN, LPAREN, RPAREN], index: 5
+    });
+    expect(lrlr([LPAREN, RPAREN, RPAREN, LPAREN, 2, 3], 0)).toBe(false);
+  });
+
+  it('many should take a parser and return a parser that matches as many times as needed', function () {
+    var l = parser.elem(LPAREN);
+    var lMany = parser.many(l);
+    expect(lMany([1, 2, LPAREN, LPAREN, LPAREN, 5 ,3, 2], 2)).toEqual({
+      tree: [LPAREN, LPAREN, LPAREN], index: 5
+    });
+    expect(lMany([1, 2, LPAREN, LPAREN, LPAREN, 5 ,3, 2], 1)).toEqual({
+      tree: [], index: 1
+    });
+    expect(lMany([1, 2, LPAREN, LPAREN, LPAREN, 5 ,3, 2], 8)).toBe(false);
+  });
+
+  it('or should take an array of parsers and return a parser that tries each till it matches', function () {
+    var l = parser.elem(LPAREN);
+    var r = parser.elem(RPAREN);
+    var either = parser.or([l, r]);
+    expect(either([5, 2, LPAREN, LPAREN, RPAREN, RPAREN, 6], 2)).toEqual({
+      tree: LPAREN, index: 3
+    });
+    expect(either([5, 2, LPAREN, LPAREN, RPAREN, RPAREN, 6], 4)).toEqual({
+      tree: RPAREN, index: 5
+    });
+    expect(either([5, 2, LPAREN, LPAREN, RPAREN, RPAREN, 6], 0)).toBe(false);
+  });
+
+
+  it('number should take one number and return it as the result', function () {
+    expect(parser.number([LPAREN, 5, 2, RPAREN], 1)).toEqual({ tree: 5, index: 2 });
+    expect(parser.number([LPAREN, 5, 2, RPAREN], 2)).toEqual({ tree: 2, index: 3 });
+    expect(parser.number([LPAREN, 5, 2, RPAREN], 0)).toBe(false);
+    expect(parser.number([LPAREN, 5, 2, RPAREN], 4)).toBe(false);
+  });
+
+  it('string should take one string and return it as the result', function () {
+    expect(parser.string([LPAREN, "Five", "Two", RPAREN], 1)).toEqual({ tree: "Five", index: 2 });
+    expect(parser.string([LPAREN, "Five", "Two", RPAREN], 2)).toEqual({ tree: "Two", index: 3 });
+    expect(parser.string([LPAREN, 5, 2, RPAREN], 0)).toBe(false);
+    expect(parser.string([LPAREN, 5, 2, RPAREN], 4)).toBe(false);
+  });
+
+
+  it('parseValueSet should parse according to ValueSet production rule', function () {
     expect(parser.parseValueSet([LPAREN, RPAREN], 0)).toEqual({ tree: [], index: 2});
     expect(parser.parseValueSet([1, 2, LPAREN, 5, 2, 3, RPAREN], 2)).toEqual({
       tree: [5, 2, 3],
@@ -18,6 +90,19 @@ describe('parser', function () {
       index: 6
     });
     expect(parser.parseValueSet([1, 5, LPAREN, 5, 2])).toBe(false);
+  });
+
+  it('parseNewState should parse according to NewState production rule', function () {
+    expect(parser.parseNewState([5, lexer.keywords._, LPAREN], 0)).toEqual({
+      tree: 5, index: 1
+    });
+    expect(parser.parseNewState([5, lexer.keywords._, LPAREN], 1)).toEqual({
+      tree: lexer.keywords._, index: 2
+    });
+
+    expect(parser.parseNewState([5, lexer.keywords._, LPAREN], 2)).toBe(false);
+    expect(parser.parseNewState([5, lexer.keywords._, LPAREN], 3)).toBe(false);
+
   });
 
 });
--- src/parser.js
+++ src/parser.js
@@ -4,28 +4,104 @@ var fs = require('fs');
 var lexer = require('./lexer.js');
 
 
-// value-set ::= ({int}) -> [{int}]
-function parseValueSet(tokens, index) {
-  var result = [];
-  if (tokens[index] != lexer.tokens.LPAREN) return false;
-  else {
-    for (var i = index + 1; i < tokens.length && (typeof tokens[i] === 'number'); i++) {
-      result.push(tokens[i]);
+// parser combinator for many
+function many(parser) {
+  return function(tokens, index) {
+    if (index >= tokens.length) return false;
+    var item;
+    var result = [];
+    while (item = parser(tokens, index)) {
+      index = item.index;
+      result.push(item.tree);
     }
-    if (i < tokens.length && tokens[i] === lexer.tokens.RPAREN)
-      return { tree: result, index: i + 1 };
+    return { tree: result, index: index };
+  };
+}
+
+// tuple2
+function tuple2(parser1, parser2, combinator) {
+  return function (tokens, index) {
+    var result1 = parser1(tokens, index);
+    if (!result1) return false;
+    if (result1.index >= tokens.length) return false;
+    var result2 = parser2(tokens, result1.index);
+    if (!result2) return false;
+    return {
+      tree: combinator(result1.tree, result2.tree),
+      index: result2.index
+    }
+  }
+}
+
+// parser that takes no tokens and always succeeds with a given result
+function empty(value) {
+  return function (tokens, index) {
+    if (index >= tokens.length) return false;
+    return {
+      tree: value,
+      index: index
+    };
+  };
+}
+
+function tupleMany(parsers) {
+  var tuple2Curried = function (parser1, parser2) {
+    return tuple2(parser1, parser2, function (a, b) {
+      a.push(b);
+      return a;
+    });
+  };
+  return parsers.reduce(tuple2Curried, empty([]));
+}
+
+function or(parsers) {
+  return function (tokens, index) {
+    var result = false;
+    for(var i = 0; i < parsers.length && !result; i++) {
+      result = parsers[i](tokens, index);
+    }
+    return result;
+  }
+}
+
+function elem(token) {
+  return function (tokens, index) {
+    if (token === tokens[index])
+      return {
+        tree: token,
+        index: index + 1
+      };
     else
       return false;
   }
 }
 
-function parseNewState(tokens, index) {
-  if (typeof tokens[index] === 'number' || tokens[index] === lexer.keywords._)
-    return  { tree: tokens[index], index: i + 1 };
-  else
-    return false;
+function number(tokens, index) {
+  if (typeof tokens[index] === 'number') return { tree: tokens[index], index: index + 1 };
+  else return false;
 }
 
+function string(tokens, index) {
+  if (typeof tokens[index] === 'string') return { tree: tokens[index], index: index + 1 };
+  else return false;
+}
+
+var parseValueSet = function(tokens, index) {
+  var result= tupleMany([elem(lexer.tokens.LPAREN), many(number), elem(lexer.tokens.RPAREN)])(tokens, index);
+  if (result) return { tree: result.tree[1], index: result.index };
+  else return false;
+}
+
+var parseNewState = or([number, elem(lexer.keywords._)]);
 
 exports.parseNewState = parseNewState;
 exports.parseValueSet = parseValueSet;
+exports.empty = empty;
+exports.elem = elem;
+exports.many = many;
+exports.tuple2 = tuple2;
+exports.tupleMany = tupleMany;
+exports.or = or;
+exports.number = number;
+exports.string = string;
+
